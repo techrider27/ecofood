e96e397aa3c7fb7004d957f9f6a37b87f36cb725
techrider27 <g.sangewar27@gmail.com>
2023-11-28 16:29:51 +0530

    inital-commit

---
+++ src/components/Body.js
@@ -0,0 +1,69 @@
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
+
+const Body = () => {
+  //local state variable
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+  const [searctText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.2513844&lng=81.62964130000002&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const toJson = await data.json();
+    // console.log(toJson);
+    setListOfRestaurant(
+      toJson?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searctText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searctText);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const newList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurant(newList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
